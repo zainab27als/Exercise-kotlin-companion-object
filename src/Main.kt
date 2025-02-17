b694f25f91a4b173a0c5b7d963122ddfc0ba78aa
Zainab 27 <zainabalsaffar03@gmail.com>
2025-02-17 19:20:33 +0300

    Main file to test the conversion

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
-    println("Hello World!")
+    val celsius = 25.0
+    val fahrenheit = TemperatureConverter.cToF(celsius)
+    println("$celsius°C = $fahrenheit°F")
+
+    val fahrenheitValue = 77.0
+    val celsiusConverted = TemperatureConverter.fToC(fahrenheitValue)
+    println("$fahrenheitValue°F = $celsiusConverted°C")
+
+    val kelvin = TemperatureConverter.cToK(celsius)
+    println("$celsius°C = $kelvin K")
+
+    val kelvinValue = 300.0
+    val celsiusFromKelvin = TemperatureConverter.kToC(kelvinValue)
+    println("$kelvinValue K = $celsiusFromKelvin°C")
 }
